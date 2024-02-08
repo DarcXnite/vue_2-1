3690ad6062db8f33eea1a7e68347bbe67b4f8ad2
DarcXnite <anhri89@yahoo.com>
2024-02-07 21:55:29 -0600

    updated commit

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-Vue.createApp({}).mount('#app')
+const data = () => {
+  return {
+    firstName: 'Davis',
+    lastName: 'Nguyen',
+    url: 'http://google.com',
+    raw_url: '<a href="http://google.com" target="_blank">Google</a>',
+    age: 20,
+  }
+}
+
+const vm = Vue.createApp({
+  data,
+  methods: {
+    fullName() {
+      return `${this.firstName} ${this.lastName.toUpperCase()}`
+    },
+    increment() {
+      this.age++
+    },
+    updateLastName(msg, e) {
+      this.lastName = e.target.value
+      console.log(msg)
+    },
+  },
+}).mount('#app')
+
+// setTimeout(() => {
+//   vm.firstName = 'Bob'
+// }, 2000)
