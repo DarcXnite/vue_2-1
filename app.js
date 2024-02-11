442643b38f4468f6d80dc7ad6f5658ac6095a89a
DarcXnite <anhri89@yahoo.com>
2024-02-10 19:08:01 -0600

    lesson 16 completed

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const data = () => {
   return {
     firstName: 'Davis',
+    middleName: '',
     lastName: 'Nguyen',
     url: 'http://google.com',
     raw_url: '<a href="http://google.com" target="_blank">Google</a>',
@@ -11,15 +12,23 @@ const data = () => {
 const vm = Vue.createApp({
   data,
   methods: {
-    fullName() {
-      return `${this.firstName} ${this.lastName.toUpperCase()}`
-    },
     increment() {
       this.age++
     },
     updateLastName(msg, e) {
       this.lastName = e.target.value
       console.log(msg)
+    },
+    updateMiddleName(e) {
+      this.middleName = e.target.value
+    },
+  },
+  computed: {
+    fullName() {
+      console.log('Full name computed prop was called')
+      return `${this.firstName} ${
+        this.middleName
+      } ${this.lastName.toUpperCase()}`
     },
   },
 }).mount('#app')
